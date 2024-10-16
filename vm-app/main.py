9994e9a737cb7702685d69d6dec18ddcfcaf46e4
StinkyBenji <ether852@gmail.com>
2024-10-16 12:55:10 +0200

    play around with flask

--- vm-app/main.py
+++ vm-app/main.py
@@ -1,24 +1,21 @@
-import os
+#!/bin/python
+from flask import Flask, render_template
+import requests
+import json
 
-port = int(os.environ.get("PORT", default=8080))
+app = Flask(__name__)
 
+def get_meme():
+    sr = "/wholesomememes"
+    url = "https://meme-api.com/gimme"
+    response = json.loads(requests.request("GET", url).text)
+    meme_large = response["preview"][-2]
+    subreddit = response["subreddit"]
+    return meme_large, subreddit
 
-async def app(scope, receive, send):
-    assert scope["type"] == "http"
+@app.route('/')
+def index():
+    meme_pic,subreddit = get_meme()
+    return render_template("index.html", meme_pic=meme_pic, subreddit=subreddit)
 
-    match scope["path"]:
-        case "/":
-            text, status = "Hello from vm-app\n", 200
-        case "/health":
-            text, status =  "UP\n", 200
-        case _:
-            text, status = "Page not found\n", 404
-
-    response_headers = [(b"content-type", b"text/plain; charset=utf-8")]
-    await send({"type": "http.response.start", "status": status, "headers": response_headers})
-    await send({"type": "http.response.body", "body": text.encode("UTF-8")})
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=port, proxy_headers=True, server_header=False, access_log=False)
+app.run(host="0.0.0.0", port=80)  
